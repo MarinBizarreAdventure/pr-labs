6db4bb3ce1d0bd494a459e5be7e1f3b77bb504c4
negai <negaimarin@gmail.com>
2024-10-24 12:53:56 +0300

    initialisation

---
+++ lab-1/lab-1/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Threading.Tasks;
+
+using lab_1.Tasks;
+
+class Program
+{
+    static async Task Main(string[] args)
+    {
+        GetRequest getRequest = new GetRequest();
+
+        string url = "https://www.cactus.md/ru/catalogue/electronice/telefone/mobilnye-telefony/";
+
+        string htmlContent = await getRequest.FetchSiteContent(url);
+        
+        Console.WriteLine(htmlContent);
+    }
+}
+
---
+++ lab-1/lab-1/Tasks/GetRequest.cs
@@ -0,0 +1,38 @@
+﻿namespace lab_1.Tasks;
+using System;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+
+public class GetRequest
+{
+    private readonly HttpClient _httpClient;
+
+    public GetRequest()
+    {
+        _httpClient = new HttpClient();
+    }
+
+    public async Task<string> FetchSiteContent(string url)
+    {
+        try
+        {
+            HttpResponseMessage response = await _httpClient.GetAsync(url);
+
+            if (response.IsSuccessStatusCode)
+            {
+                string htmlContent = await response.Content.ReadAsStringAsync();
+                return htmlContent;
+            }
+            else
+            {
+                return $"Error: Unable to fetch the website. StatusCode: {response.StatusCode}";
+            }
+        }
+        catch (Exception ex)
+        {
+            return $"Exception: {ex.Message}";
+        }
+    }
+
+}
